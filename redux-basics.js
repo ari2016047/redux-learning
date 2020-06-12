021f774ef3ab60c8084b8736cbb6a25a9d91164c
ari201604d7 <2016047@iiitdmj.ac.in>
2020-06-12 22:59:28 +0530

    Connect React to Redux, connecting store to react

--- redux-basics.js
+++ redux-basics.js
@@ -1,3 +1,4 @@
+//to run > node redux-basics.js
 const redux = require('redux');
 const createStore = redux.createStore;
 
@@ -27,9 +28,19 @@ const rootReducer = (state = initialState, action) =>{
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+
+//Subscription
+//store triggers subscription on any change in state/whenver action is dispactched
+//{ counter: 0 }
+//Subscription  { counter: 1 }
+//Subscription  { counter: 11 }
+//{ counter: 11 }
+store.subscribe(()=>{
+    console.log('Subscription ',store.getState());
+});
+
 //Dispatching Action
 store.dispatch({type:'INC_COUNTER'});
 store.dispatch({type:'ADD_COUNTER', value: 10});
 console.log(store.getState());
 
-//Subscription
